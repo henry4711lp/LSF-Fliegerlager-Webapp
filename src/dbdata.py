7724a4badd5ae683c3ddc22e8ee266cf26f1a429
Jan <henry4711lp@gmail.com>
2022-09-30 17:30:33 +0200

    Update dbdata.py

--- src/dbdata.py
+++ src/dbdata.py
@@ -1,13 +1,13 @@
 from src.connection import dbconnector, vf_data
 
 
-def get_id_by_vname_and_nname(VNAME, NNAME):
-    sql_statement = "SELECT ID FROM GETR WHERE VNAME = '" + VNAME + "' AND NNAME = '" + NNAME + "'"
+def get_id_by_vname_and_nname(vname, nname):
+    sql_statement = "SELECT ID FROM GETR WHERE VNAME = '" + vname + "' AND NNAME = '" + nname + "'"
     return dbconnector.sql(sql_statement)
 
 
-def get_vfid_by_vname_and_nname_from_VF(VNAME, NNAME):
-    return vf_data.get_vfid(VNAME, NNAME)
+def get_vfid_by_vname_and_nname_from_VF(vname, nname):
+    return vf_data.get_vfid(vname, nname)
 
 
 def get_EID_by_date(date):
@@ -20,11 +20,11 @@ def get_GPreis_by_GID(gid):
     return dbconnector.sql(sql_statement)
 
 
-def get_PERSESS_by_ID_and_EID(id, eid):
-    sql_statement = "SELECT * FROM PERSESS WHERE ID = '" + id + "' AND EID = '" + eid + "'"
+def get_PERSESS_by_ID_and_EID(pid, eid):
+    sql_statement = "SELECT * FROM PERSESS WHERE ID = '" + pid + "' AND EID = '" + eid + "'"
     return dbconnector.sql(sql_statement)
 
 
-def get_PERSGET_by_ID_and_GID(id, gid):
-    sql_statement = "SELECT * FROM PERSGET WHERE ID = '" + id + "' AND GID = '" + gid + "'"
+def get_PERSGET_by_ID_and_GID(pid, gid):
+    sql_statement = "SELECT * FROM PERSGET WHERE ID = '" + pid + "' AND GID = '" + gid + "'"
     return dbconnector.sql(sql_statement)

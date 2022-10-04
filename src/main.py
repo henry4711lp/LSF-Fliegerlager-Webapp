3045c4a05c09c129be678c1e9fa7b0e0d6929740
Robin <robin.busch@vodafone.com>
2022-10-04 21:56:26 +0200

    register and home page added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from src.connection import dbconnector
 
 # create flask app
@@ -6,8 +6,22 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def connection():
-    return dbconnector.sql("SELECT * FROM GETR")
+def index():
+    return render_template("start.html")
+
+
+@app.route('/register')
+def index():
+    return render_template("register.html")
+
+
+@app.route("/home", methods=["POST", "GET"])
+def home():
+    name = ""
+    if request.method == "POST":
+        name = request.form["vname"]
+
+    return f"Hello {name}!"
 
 
 if __name__ == '__main__':

58c53b99947ef3f6418ebed6c82d8088c0a7909d
Robin <robin.busch@vodafone.com>
2022-10-10 19:17:57 +0200

    added dynamic text -> Name, ID on /home

--- src/main.py
+++ src/main.py
@@ -36,8 +36,9 @@ def home():
         ID = int(json.loads(json.dumps(ID))[0][0])
 
         today = date.today().strftime("%d.%m.%Y")
+        display_ID = str(ID)
 
-        resp = make_response(render_template("home.html", ID=ID, date=today))
+        resp = make_response(render_template("home.html", ID=ID, date=today, display_name=vname, display_ID=display_ID))
         resp.set_cookie("UserID", f'{ID}')
         # Man könnte hier noch eine Ablaufzeit für die Cookies setzen mit resp.set_cookie(
         # "UserID", f'{ID}', max_age=<ExpirationTime>))

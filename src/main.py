31d0a0681687b31a4f68d8abbd8c73479e9ee734
rbn-de <robin.busch2000@gmx.de>
2022-10-12 21:47:41 +0200

    Changed Start-page Route (instant login)

--- src/main.py
+++ src/main.py
@@ -13,12 +13,13 @@ UserID = 0
 
 @app.route('/')
 def index():
-    return render_template("start.html")
-
-
-@app.route('/register')
-def register():
+    # return render_template("start.html")
     return render_template("register.html")
+
+
+# @app.route('/register')
+# def register():
+#     return render_template("register.html")
 
 
 @app.route("/home", methods=["POST", "GET"])  # TODO: Display VName in HTML

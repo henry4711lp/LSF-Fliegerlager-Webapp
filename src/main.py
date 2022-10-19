6d7baf6e5a868cac76632cf408de52d19a325477
Jan <henry4711lp@gmail.com>
2022-10-19 21:50:14 +0200

    Revert "fix Cross-Site Request Forgery vuln"
    This reverts commit 56241e458ee98bc761cea028882230a50e21fdbc.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,12 @@
 import json
 import logging
-
-import flask
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import date
-from src import dbdata, webwork
+from src import dbdata, tablegenerator, formatprices, webwork
 from src.connection import dbconnector
-from flask_wtf.csrf import CSRFProtect
 
 # create flask app
 app = Flask(__name__, static_url_path='/static')
-csrf = CSRFProtect()
-csrf.init_app(app)
 UserID = 0
 
 

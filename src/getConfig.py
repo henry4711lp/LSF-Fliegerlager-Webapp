d13040bb17097e5ff1f520e6f24aa4f904a61c5f
Jan.Sellerbeck <henry4711lp@gmail.com>
2022-10-24 16:33:44 +0200

    implemented tests over actions

--- src/getConfig.py
+++ src/getConfig.py
@@ -1,8 +1,10 @@
 # get data from config yaml file
+import os.path
+
 import yaml
 
 
 def get_config(x):
-    with open("../config/config.yaml", "r") as file:
+    with open(os.path.abspath("../config/config.yaml"), "r") as file:
         data = yaml.safe_load(file)
     return data.get(x)

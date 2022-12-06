e89a785dddec3a491411be7367e4389d7e15cb70
Jan Sellerbeck <36828267+henry4711lp@users.noreply.github.com>
2022-12-06 16:36:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='Fliegerlager Web App',
-    description='This app is to replace a handwritten list for "bought" drinks, dinners and accomodation days. The '
-                'purpose of this webapp is to decrease the workload of the club youth management in the accounting '
-                'process of the flying camp and to prevent the loss of the handwritten lists. ',
+    description='This app is to replace a handwritten list for "bought" drinks, dinners and accomodation days. The purpose of this webapp is to decrease the workload of the club youth management in the accounting process of the flying camp and to prevent the loss of the handwritten lists.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='henry4711lp',
     url='',
     packages=find_packages('src'),
@@ -18,4 +20,10 @@ setup(
     entry_points={
         'console_scripts': [
             'web_server = app:main']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )

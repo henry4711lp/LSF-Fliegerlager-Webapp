d0f9dfffb0c89395698146bcc2ea03fcf5d9ff82
Jan Sellerbeck <36828267+henry4711lp@users.noreply.github.com>
2022-12-06 16:45:45 +0100

    chat-gpt testcases

--- src/test_basics.py
+++ src/test_basics.py
@@ -1,74 +1,87 @@
-import unittest
-import src.main as tested_app
+from unittest.mock import Mock
 
+from flask import Flask, redirect, make_response
+from flask.testing import FlaskClient
+def test_index():
+    # Arrange
+    app = Flask(__name__)
+    
+    with app.test_request_context():
+        # Act
+        result = index()
+        
+        # Assert
+        assert isinstance(result, redirect)
+        assert result.location == url_for('register')
 
-class FlaskAppTests(unittest.TestCase):
+def test_register():
+    # Arrange
+    app = Flask(__name__)
+    
+    with app.test_request_context():
+        # Act
+        result = register()
+        
+        # Assert
+        assert isinstance(result, make_response)
+        assert "register.html" in result.response
+        assert "UserID=0" in result.cookies
+        assert result.cookies["UserID"].max_age == 0
 
-    def setUp(self):
-        tested_app.app.config['TESTING'] = True
-        self.app = tested_app.app.test_client()
+def test_home():
+    # Arrange
+    app = Flask(__name__)
+    with app.test_request_context(method="POST", referrer="/register"):
+        webwork.signup_in = Mock(return_value="signup_in_result")
+        
+        # Act
+        result = home()
+        
+        # Assert
+        assert result == "signup_in_result"
+        
+    with app.test_request_context(method="POST", referrer="/stays"):
+        webwork.stay = Mock(return_value="stay_result")
+        
+        # Act
+        result = home()
+        
+        # Assert
+        assert result == "stay_result"
+        
+    with app.test_request_context(method="POST", referrer="/drinkselector"):
+        webwork.drink = Mock(return_value="drink_result")
+        
+        # Act
+        result = home()
+        
+        # Assert
+        assert result == "drink_result"
+        
+    with app.test_request_context(method="GET"):
+        # Act
+        result = home()
+        
+        # Assert
+        assert result[1] == 404
 
-    def test_root_endpoint_fwd(self):
-        r = self.app.get('/')
-        self.assertEqual(r.status_code, 302)
-        print("tested root forward")
-
-    def test_register_endpoint(self):
-        r = self.app.get('/register')
-        self.assertEqual(r.status_code, 200)
-        print("tested register 200")
-
-    def test_home_error_on_no_post(self):
-        r = self.app.get('/home')
-        self.assertEqual(r.status_code, 404)
-        print("tested error on no post home")
-
-    def test_home_post_login_data(self):
-        print("tested post with test data to home")
-
-    # def test_post_hello_endpoint(self):
-    #     r = self.app.post('/')
-    #     self.assertEqual(r.status_code, 405)
-    #
-    # def test_get_api_endpoint(self):
-    #     r = self.app.get('/api')
-    #     self.assertEqual(r.json, {'status': 'test'})
-    #
-    # def test_correct_post_api_endpoint(self):
-    #     r = self.app.post('/api',
-    #                       content_type='application/json',
-    #                       data=json.dumps({'name': 'Den', 'age': 100}))
-    #     self.assertEqual(r.json, {'status': 'OK'})
-    #     self.assertEqual(r.status_code, 200)
-    #
-    #     r = self.app.post('/api',
-    #                       content_type='application/json',
-    #                       data=json.dumps({'name': 'Den'}))
-    #     self.assertEqual(r.json, {'status': 'OK'})
-    #     self.assertEqual(r.status_code, 200)
-    #
-    # def test_not_dict_post_api_endpoint(self):
-    #     r = self.app.post('/api',
-    #                       content_type='application/json',
-    #                       data=json.dumps([{'name': 'Den'}]))
-    #     self.assertEqual(r.json, {'status': 'bad input'})
-    #     self.assertEqual(r.status_code, 400)
-    #
-    # def test_no_name_post_api_endpoint(self):
-    #     r = self.app.post('/api',
-    #                       content_type='application/json',
-    #                       data=json.dumps({'age': 100}))
-    #     self.assertEqual(r.json, {'status': 'bad input'})
-    #     self.assertEqual(r.status_code, 400)
-    #
-    # def test_bad_age_post_api_endpoint(self):
-    #     r = self.app.post('/api',
-    #                       content_type='application/json',
-    #                       data=json.dumps({'name': 'Den', 'age': '100'}))
-    #     self.assertEqual(r.json, {'status': 'bad input'})
-    #     self.assertEqual(r.status_code, 400)
-    #
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_stays():
+    # Arrange
+    app = Flask(__name__)
+    app.get_uid_from_cookie = Mock(return_value=1)
+    dbdata.get_vname_by_id = Mock(return_value="VName")
+    dbdata.get_nname_by_id = Mock(return_value="NName")
+    dbdata.get_stay_counter_by_id = Mock(return_value=2)
+    dbdata.get_stay_start_end_by_id = Mock(return_value=("start", "end"))
+    
+    with app.test_request_context():
+        # Act
+        result = stays()
+        
+        # Assert
+        assert "stays.html" in result.response
+        assert "VName" in result.response
+        assert "NName" in result.response
+        assert "start" in result.response
+        assert "end" in result.response
+        assert "counter=2" in result.response

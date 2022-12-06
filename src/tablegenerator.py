c7b118e063b407ce3982be8af7fabca7107d20e8
Jan Sellerbeck <36828267+henry4711lp@users.noreply.github.com>
2022-12-06 15:25:12 +0100

    added docstring

--- src/tablegenerator.py
+++ src/tablegenerator.py
@@ -4,6 +4,10 @@ from tabulate import tabulate
 
 
 def get_table(data):
+     """
+    This function uses the `tabulate` library to convert the given data into an HTML table.
+    If an IndexError is raised, a message is logged with the `logging` module and the function returns 0.
+    """
     tabulate.PRESERVE_WHITESPACE = True
     try:
         table = tabulate(data, headers=['Datum', '     Anzahl'], tablefmt='html', colalign=("left", "right"), numalign="right")

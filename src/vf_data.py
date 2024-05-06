302a4051f0efa4ee7f114a664da51bec76670ed1
Jan <Jan.Sellerbeck@Vodafone.com>
2024-05-06 19:59:35 +0200

    Changes by Jan, added docstrings and disabled 2fa, not practicable, enabled full get_vfid functionality and caught 400/401 error

--- src/vf_data.py
+++ src/vf_data.py
@@ -7,6 +7,20 @@ import getConfig
 
 # Get the data from the API
 def get_access_token():
+    """
+        This function is used to get the access token from the API.
+
+        It sends a GET request to the API endpoint for access tokens.
+        The function returns the access token if the request is successful.
+
+        Raises:
+        ConnectionRefusedError: If the server returns a 401 status code, indicating unauthorized access.
+        ConnectionError: If the server returns a 500 or greater status code, indicating an internal server error.
+        ConnectionError: If the server returns any other status code.
+
+        Returns:
+        str: The access token if the request is successful.
+        """
     url = _api_url + "interface/rest/auth/accesstoken"
     response = requests.get(url)
     data = response.json()
@@ -25,10 +39,28 @@ def test():
 
 
 def login():
+    """
+        This function is used to log in a user using the API.
+        WARNING: The password is hashed using MD5 before being sent.
+        WARNING: The password is stored in plain text in the config file.
+        WARNING: 2FA is implemented but not working with a functional user.
+
+        It sends a POST request to the API with the username, password, and API token.
+        The password is hashed using MD5 before being sent.
+        The function returns the access token if the login is successful.
+
+        Raises:
+        ConnectionRefusedError: If the server returns a 401 status code, indicating unauthorized access.
+        ConnectionError: If the server returns a 500 or greater status code, indicating an internal server error.
+        ConnectionError: If the server returns any other status code.
+
+        Returns:
+        str: The access token if the login is successful.
+        """
     logging.info('logging user ' + _api_username + ' in...')
     # post to api with username and password and api_token
     url = _api_url + "interface/rest/auth/signin"
-    auth_secret = input('Enter auth_secret: ')
+    #auth_secret = input('Enter auth_secret: ')
     accesstoken = get_access_token()
     password = hashlib.md5(_api_password.encode()).hexdigest()
     payload = {
@@ -37,7 +69,7 @@ def login():
         "password": password,
         "appkey": _api_token,
         "cid":  _api_cid,
-        "auth_secret": auth_secret
+        #"auth_secret": auth_secret
     }
     logging.debug('Password: ')
     logging.debug(password)
@@ -55,15 +87,30 @@ def login():
 
 
 def get_vfid(vname, nname):
+    """
+        This function is used to get the member ID of a user by their first and last name.
+        IMPORTANT: THIS FUNCTION REQUIRES THE RIGHT "Mitgliederdaten bearbeiten"/"Edit member data"
+
+        Parameters:
+        vname (str): The first name of the user.
+        nname (str): The last name of the user.
+
+        Returns:
+        int: The member ID of the user if found, otherwise 1.
+        """
     logging.info('getting vfid for ' + vname + ' ' + nname + '...')
     try:
         accesstoken = login()
-        url = _api_url + "interface/rest/auth/getuser"
+        url = _api_url + "interface/rest/user/list"
         payload = {
             'accesstoken': accesstoken,
         }
         response = requests.post(url, data=json.dumps(payload))
-        return response.json().get("memberid")
+        if response.status_code != 200:
+            raise ConnectionError("Server returned " + str(response.status_code))
+        for user in response.json().get("users"):
+            if user.get("firstname") == vname and user.get("lastname") == nname:
+                return user.get("memberid")
     except ConnectionError:
-        logging.error("Error while getting vfid")
+        logging.error("Error while getting vfid returning internal ID")
         return 1
